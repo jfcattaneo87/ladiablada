13bc1a5ade91ae7fdeb8a1fbaaa453afa0157a7f
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-13 15:39:13 -0500

    agregando navBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
         <p>
           hello world
         </p>
+        <NavBar></NavBar>
     </div>
   );
 }
