6a0c61553847dbdc1f555c823acd6db011004ad6
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-13 16:26:10 -0500

    agregando navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,27 @@
 import './NavBar.css';
+// external components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const NavBar = () => {
 
   return (
-    <div className="NavBar">
-        <p>
-          NavBar
-        </p>
+    <div className="main-NavBar">
+         <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
