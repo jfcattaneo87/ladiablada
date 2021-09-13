056f6c1564a46b04a192b8875f73c24c0128108e
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-13 16:28:57 -0500

    agregando navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,10 +13,10 @@ const NavBar = () => {
     <div className="main-NavBar">
          <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start"  color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6">
             News
           </Typography>
           <Button color="inherit">Login</Button>
