2249e9076cbea48e17128023ca1978d2efd8d780
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-19 22:25:02 -0500

    cambiando nombre a list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import CartWidget from './components/CartWidget/CartWidget';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ListContainer from './components/ItemListContainer/ItemListContainer'
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
         <CartWidget/>
         <NavBar/>
-        <ItemListContainer/>
+        <ListContainer/>
         
     </div>
   );
