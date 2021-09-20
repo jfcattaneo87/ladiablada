3cd1314a593804d2a6448d48b3a61a5d89136821
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-19 22:14:11 -0500

    modificando estilos de cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import CartWidget from './components/CartWidget/CartWidget';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
         <CartWidget/>
+        <NavBar/>
+        <ItemListContainer/>
+        
     </div>
   );
 }
