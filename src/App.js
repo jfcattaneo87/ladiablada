e2d568783a22668a5208488fd87e65409e9b101d
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-19 21:36:24 -0500

    cartwidget nuevo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import CartWidget from './components/CartWidget/CartWidget';
 
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <CartWidget/>
     </div>
   );
 }
