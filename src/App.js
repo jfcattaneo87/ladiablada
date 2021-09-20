63ac382e6e8beaddb0087950b3e1c6d89ac8041c
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-20 00:55:36 -0500

    modificando app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Products from './components/Products/Products';
 import CartWidget from './components/CartWidget/CartWidget';
 import ListContainer from './components/ListContainer/ListContainer';
 
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
         <CartWidget/>
         <NavBar/>
-        <Products/>
         <ListContainer/>
 
     </div>
--- src/components/ListContainer/ListContainer.js
+++ src/components/ListContainer/ListContainer.js
@@ -1,35 +1,17 @@
+import React from "react";
 import './ListContainer.css';
+import Products from "../Products/Products";
 // external components
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 
 
-
-const ListContainer = () => {
-
-    return (
-        <AppBar position="static" className="main-NavBar">
-        <Toolbar>
-            <div className="container-logo">
-                <Typography variant="h5">
-                    La Diablada
-                </Typography>
-            </div>
-            <div className="container-icono-login">
-                <Button color="inherit">inicio</Button>
-                <Button color="inherit">about</Button>
-                <Button color="inherit">products</Button>
-                <Button color="inherit">Login</Button>
-            </div>
-        </Toolbar> 
-  </AppBar>
-        
-        
-    );
+  export default class ListContainer extends React.Component {
+      render(){
+          return(
+              <div>
+                   <Products title="combo 1" price="35.00"/>
+              </div>
+          )
+      }
   }
-  
-  export default ListContainer;
   
