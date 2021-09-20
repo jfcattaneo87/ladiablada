968ec6d3129b6835382f6e9597a81717a777c7aa
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-19 21:36:54 -0500

    nuevos npm icon

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,20 @@
+import './CartWidget.css';
+import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartPlus, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+
+
+const CartWidget = () => {
+
+    
+    return (
+                  <div className="cart-buttonNav">
+                    <Button variant="text">
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        <p>1</p>
+                    </Button>
+                  </div>  
+    );
+  }
+  
+  export default CartWidget;
