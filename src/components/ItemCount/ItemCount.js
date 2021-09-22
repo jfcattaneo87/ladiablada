282f85d836838f9272beba870cba1cb675cd315b
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-22 13:58:13 -0500

    agregando call onAdd

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -2,19 +2,25 @@ import React,{useState} from "react";
 
 export default function ItemCount(props) {
     const [Items, setItems] = useState(0)
+    const [Stock, setStock] = useState(5)
+    const [disableButton, setDisableButton] = useState(false)
 
-    const sumar = () => {
-        setItems(items +1)
+    const onAdd = () => {
+        if(Items < Stock){
+            setItems(Items +1)
+        }else{
+            setDisableButton(true)
+        }
     }
     const restar = () => {
-        setItems(items -1)
+        setItems(Items -1)
     }
+
     return(
         <div>
             <button onClick={restar}>-</button>
             {Items}
-            <button onClick={sumar}>+</button>
-
+            <button disabled={disableButton} onClick={onAdd}>+</button>
         </div>
     )
 }
