227dfa571760446e04941ede986ac72aa8010a67
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-22 13:10:45 -0500

    agregando items

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,10 +1,19 @@
 import React,{useState} from "react";
 
-export default function ItemCount(props){
+export default function ItemCount(props) {
+    const [Items, setItems] = useState(0)
+
+    const sumar = () => {
+        setItems(items +1)
+    }
+    const restar = () => {
+        setItems(items -1)
+    }
     return(
         <div>
-            <button>-</button>
-            <button>+</button>
+            <button onClick={restar}>-</button>
+            {Items}
+            <button onClick={sumar}>+</button>
 
         </div>
     )
