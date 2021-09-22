a4c79c2d2218295d1aac81efa15ac73053a6fa98
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-22 12:48:49 -0500

    ItemCount

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+
+export default function ItemCount(props){
+    return(
+        <div>
+            <button>-</button>
+            <button>+</button>
+
+        </div>
+    )
+}
