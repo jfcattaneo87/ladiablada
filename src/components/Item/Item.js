fb827aea00e447e3bb314a5dbe0e4ad9649f391d
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-27 00:44:00 -0500

    modificando componentes de img

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Item.css'; 
+import imagenes from '../imgenes/uno.jpg';
+import imagenes from '../imgenes/dos.jpg';
+import imagenes from '../imgenes/tres.jpg';
 
 
 
@@ -8,9 +11,11 @@ export const Item = ({ item }) => {
   
   return <div className="Item">
                 <div>
-                    <img src={`../img${item.img}`}/>
+                    <img src={imagenes}/>
                 </div>
                     <h2>{item.title}</h2>
                     <p>${item.price}</p>    
         </div>;
 };
+
+//<img src={`../img/uno.jpg${item.img}`}/>
