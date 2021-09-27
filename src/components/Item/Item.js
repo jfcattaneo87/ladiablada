71b30b8c71151ecc02f82e4a84d941ebf2da99d8
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-26 23:38:16 -0500

    cambiando la ruta de item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export const Item = ({ item }) => {
-  // aca mostrarian todas las partes del item/producto en cuestion, dandole estilo
+  
   return <div>
-                <div>{item.img}
+                <div>
                     <img src={`./assets/img/${item.img}`}/>
                 </div>
-                <h2>{item.title}</h2>
-                <p>${item.price}</p>    
+                    <h2>{item.title}</h2>
+                    <p>${item.price}</p>    
         </div>;
 };
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,19 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import './ItemListContainer.css';
-import { ItemList } from './ItemList';
+import { ItemList } from '../ItemList/ItemList';
 // external components
 
 export const ItemListContainer = () => {
     const [items, setItems] = useState([]);
   
     useEffect(() => {
-      // esta seria su lista de items. deberian tener un titulo o nombre, foto, precio. piensen que son como cards asi que no llevan tantos datos.
-  
-      // recomiendo poner las fotos en la carpeta "public" en una subcarpeta img o imagenes o assets. asi lo llaman directamente en el src de la imagen ---> <img src="/img/miImagen.jpg" alt="mi foto" />
-  
-      // entonces de esa forma la ruta de la imagen empieza con una barra siempre, no lleva punto adelante, se pone el nombre de la carpeta y el nombre del archivo.
-  
-      // si tienen imagenes en src las tienen que importar y es un lio
+      
       const list = [{ id: '001',
                       title: 'combo 1', 
                       price:'1000', 
@@ -30,21 +24,21 @@ export const ItemListContainer = () => {
                       img:'tres.jpg'}
                     ];
   
-      // esta es la promise que se resuelve en 2 segundos
+      
       const getItems = new Promise((resolve) => {
         setTimeout(() => {
-          // hacemos el resolve de nuestro array. no hace falta el reject
+          
           resolve(list);
         }, 2000);
       });
   
-      // traemos los datos de nuestra promise y los ponemos en el estado
+      
       getItems.then((res) => {
         setItems(res);
       });
     }, []);
   
-    // el contenedor llama al componente presentacion ItemList
+    
     return <ItemList items={items} />;
   };
   
