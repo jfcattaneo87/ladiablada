90c5ad490c91f31b6eae91849415a3fefa3bee51
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-26 23:53:36 -0500

    agregando estilos

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Item } from '../Item/Item';
+import './ItemList.css'; 
 
 export const ItemList = ({ items }) => {
 
   return (
-    <div>
+    <div className="item-List">
       {items.map((item) => {
         return( <Item item={item} />)
         })}
