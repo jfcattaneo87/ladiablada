ef90e11b258309b5d0da2a7540805478a3f3de9c
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-09-27 00:24:01 -0500

    moviendo carpeta de imagenes

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Item.css'; 
 
+
+
+
 export const Item = ({ item }) => {
   
   return <div className="Item">
                 <div>
-                    <img src={`${item.img}`}/>
+                    <img src={`../img${item.img}`}/>
                 </div>
                     <h2>{item.title}</h2>
                     <p>${item.price}</p>    
