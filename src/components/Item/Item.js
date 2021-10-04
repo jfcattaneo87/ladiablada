a9952be5a5e11626c390ac969d55f3dfdb2099ea
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-10-03 21:42:13 -0400

    cambiando de lugar itemCount

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Item.css';
 import ItemDetail from '../ItemDetail/ItemDetail.js';
+import ItemCount from '../ItemCount/ItemCount.js';        
 
 import { Link } from 'react-router-dom'
 
@@ -11,7 +12,7 @@ export const Item = ({ item }) => {
     <Link to={`/itemDetail/${item.id}`}>
       <div className="Item">
         <ItemDetail item={item} />
-        
+        <ItemCount stock={5} initial={1}/>
       </div>
       
     </Link>
